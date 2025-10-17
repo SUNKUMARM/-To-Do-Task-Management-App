d8a1ea8799bb0ba4d4ced7572f76de0050e21ebe
Sunkumar M <sunkumarm.mayilvel@gmail.com>
2025-10-18 00:05:28 +0530

    component initialize

---
+++ src/app/component/todo-task-dialog/todo-task-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'create-task-dialog',
+    templateUrl: './todo-task-dialog.component.html',
+    styleUrls: ['']
+})
+
+export class TodoTaskCreateDialog {
+
+}
